ae0674624102ae5b09600196ac5bf62ba72aad27
plasmashadow <plasmashadowx@gmail.com>
2015-09-24 12:05:32 +0530

    INitial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+##Thread.js
+
+Theading Implementation using HTML5 WorkerAPI.
+
+
+##Installation
+
+```
+#through npm
+
+git clone https://github.com/plasmashadow/worker.js
+
+cd worker.js/
+
+npm install
+
+#on browser
+
+<script src="thread.js"></script>
+
+```
+
+##Usage
+
+
+###Creating Threads
+
+```
+
+var thread = new Thread();
+
+var childid = thread.addTask(function(a){
+  for(var i=0; i< 100; i++){
+    cosole.log(a);
+  }
+  }, "sathya");
+
+
+var childid2 = thread.addTask(function(a){
+    for(var i=0; i< 100; i++){
+      cosole.log(a);
+    }
+    }, "adhi");
+
+thread.start();
+
+```
+
+###Event handling
+
+Thread Events can be managed by their respective events.
+
+```
+var thread = new Thread();
+
+thread.once('message', function(message){
+  console.log(message);
+})
+
+thread.once('close', function(){
+  console.log("thread is ending");
+})
+
+thread.once('error', function(error){
+  console.log(error);
+})
+
+```
+
+###Posting Message to Child Threads
+
+Posting message can to the childs can be done with there childids.
+
+```
+
+thread.message(childid, "please work fast");
+
+
+```
+
+
+##License  
+MIT
---
+++ index.js
@@ -0,0 +1,48 @@
+var Emitter = require('events').EventEmitter;
+
+
+
+function Process(fn, params) {
+    this.process = fn;
+    var URL = window.URL;
+    //http://stackoverflow.com/questions/5408406/web-workers-without-a-separate-javascript-file
+    this.blob = URL.createObjectURL(new Blob(['(', fn, ').apply(null,[', params, '])'], {
+        type: 'application/javascript'
+    }))
+    return this.blob;
+}
+
+module.exports = Thread = function(){
+  this.tasks = [];
+  this.workers = [];
+  this.worker_maps = {};
+}
+
+Thread.prototype = new Emitter;
+
+Thread.prototype.addTask = function(fn, params){
+  var task = Process(fn, params);
+  var obj = [new Date().getTime(),task];
+  this.tasks.push(obj);
+  return obj[0];
+}
+
+
+Thread.prototype.start = function(){
+  var that = this;
+  this.tasks.forEach(function(tsk){
+    var worker = new Worker(tsk[1]);
+    that.worker_maps[tsk[0]] = worker;
+    worker.onmessage = function(message){
+      that.emit('message', message);
+    }
+    worker.onclose = function(data){
+      that.emit('close', data);
+    }
+    that.workers.push(worker);
+  });
+}
+
+Thread.prototype.message =function(id, message){
+  this.worker_maps[id].postMessage(message);
+}
